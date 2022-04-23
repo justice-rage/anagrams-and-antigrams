51e89f18087ef7e2de811bfb429546fcfd4f11ca
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-23 16:31:14 -0700

    add script file

--- anagram_script.rb
+++ anagram_script.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/ruby
 require("./lib/anagram.rb")
+
+puts""
+puts "Welcome to the Anagram Checker."
+puts "Your two inputs will be compared to check if they are anagrams, antigrams, or words at all."
+puts""
+puts "Enter a word or sentence:"
+word1 = gets.chomp
+puts
+
+puts "Enter a second word or sentence:"
+word2 = gets.chomp
+puts
+
+input = AnagramChecker.new(word1, word2)
+
+if input.anagram === "The pair are not words." || input.anagram === "The pair are anagrams." || input.anagram === "The pair are antigrams."
+  puts input.anagram
+end
