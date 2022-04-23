4f81cf5f132485edbfaa726793cb3ca60e049d64
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-23 12:23:07 -0700

    add initialize and reader methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,13 @@
 #!/usr/bin/ruby
 
-puts "Hello world!"
+class AnagramChecker
+  attr_reader (:word1, :word2)
+
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+end
+
+
+# puts "Hello world!"
