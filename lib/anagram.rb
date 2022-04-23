3821f73b0f7ed6228f54beb55a4f1d70fbdfc160
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-23 16:03:07 -0700

    pass check if sentences are anagram test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,10 +5,10 @@ class AnagramChecker
   attr_reader :word1, :word2
 
   def initialize(word1, word2)
-    @word1 = word1.downcase
-    @word2 = word2.downcase
+    @word1 = word1.downcase.delete(' ').gsub(/[^0-9A-Za-z]/, '')
+    @word2 = word2.downcase.delete(' ').gsub(/[^0-9A-Za-z]/, '')
   end
-
+# binding.pry
   def anagram()
     if !(@word1 =~ /[aeiou]/) && !(@word2 =~ /[aeiou]/)
     "The pair are not words."
