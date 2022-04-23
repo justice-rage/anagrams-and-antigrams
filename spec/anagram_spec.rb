09110e927e98aa7f43eaee94a37175b0c2a87b39
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-23 12:50:42 -0700

    edit expect to equal statement

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,6 +5,6 @@ describe (AnagramChecker) do
   describe('#anagram') do
     it("will check if word1 and word2 are anagrams") do
       word_pair = AnagramChecker.new("ruby", "bury")
-      expect(word_pair.anagram).to(eq("The words are anagrams"))
+      expect(word_pair.anagram).to(eq("The pair are anagrams."))
     end
 end
