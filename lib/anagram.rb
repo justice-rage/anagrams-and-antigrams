9231dc46a30be65ff13840ccc3cd3fc91dd517d3
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-23 15:11:22 -0700

    add downcase method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,16 +5,16 @@ class AnagramChecker
   attr_reader :word1, :word2
 
   def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
+    @word1 = word1.downcase
+    @word2 = word2.downcase
   end
 
   def anagram()
     if !(@word1 =~ /[aeiou]/) && !(@word2 =~ /[aeiou]/)
     "The pair are not words."
-    elsif (@word1.chars.sort(&:casecmp) == @word2.chars.sort(&:casecmp))
+    elsif (@word1.chars.sort == @word2.chars.sort)
       "The pair are anagrams."
-    else (@word1.chars.sort(&:casecmp) != @word2.chars.sort(&:casecmp))
+    else (@word1.chars.sort != @word2.chars.sort)
       "The pair are antigrams."
     end
   end
