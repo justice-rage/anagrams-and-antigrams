95210243697093330bddc0ee7ca970bc93dea82d
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-23 13:27:09 -0700

    create test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,9 +3,14 @@ require('anagram')
 
 describe (AnagramChecker) do
   describe('#anagram') do
-    it("will check if word1 and word2 are anagrams") do
-      word_pair = AnagramChecker.new("ruby", "bury")
-      expect(word_pair.anagram).to(eq("The pair are anagrams."))
-    end
+  it("will check if word1 and word2 are anagrams") do
+    word_pair = AnagramChecker.new("ruby", "bury")
+    expect(word_pair.anagram).to(eq("The pair are anagrams."))
   end
+
+  it("will check if word1 and word2 are antigrams") do
+    word_pair = AnagramChecker.new("light", "dark")
+    expect(word_pair.anagram).to(eq("The pair are antigrams."))
+  end
+end
 end
