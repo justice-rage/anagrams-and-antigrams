ff6f831ab7f73dbd0b1e9ac203b114c9d51bd52e
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-23 15:30:28 -0700

    create check if sentences are anagram test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,19 +7,24 @@ describe (AnagramChecker) do
     word_pair = AnagramChecker.new("ruby", "bury")
     expect(word_pair.anagram).to(eq("The pair are anagrams."))
   end
-
+  
   it("will check if word1 and word2 are antigrams") do
     word_pair = AnagramChecker.new("light", "dark")
     expect(word_pair.anagram).to(eq("The pair are antigrams."))
   end
-
+  
   it("will check if word1 and word2 are words") do
     word_pair = AnagramChecker.new("lght", "drk")
     expect(word_pair.anagram).to(eq("The pair are not words."))
   end
-
+  
   it("will check if lowercase word1 and uppercase word2 are anagrams.") do
     word_pair = AnagramChecker.new("RUBY", "bury")
+    expect(word_pair.anagram).to(eq("The pair are anagrams."))
+  end
+
+  it("will check if sentence inputs for word1 and word2 are anagrams") do
+    word_pair = AnagramChecker.new("The Morse Code", "Here come dots!")
     expect(word_pair.anagram).to(eq("The pair are anagrams."))
   end
 end
