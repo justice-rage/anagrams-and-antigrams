bc78aed32121f5dc3034beeeae544d8f437ec3dc
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-23 15:09:57 -0700

    create uppercase vs lowercase anagram check test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -17,5 +17,10 @@ describe (AnagramChecker) do
     word_pair = AnagramChecker.new("lght", "drk")
     expect(word_pair.anagram).to(eq("The pair are not words."))
   end
+
+  it("will check if lowercase word1 and uppercase word2 are anagrams.") do
+    word_pair = AnagramChecker.new("RUBY", "bury")
+    expect(word_pair.anagram).to(eq("The pair are anagrams."))
+  end
 end
 end
